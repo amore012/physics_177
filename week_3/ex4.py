642f00ccc87486e2d3c33c5bd2ded5b767bc81bf
amore012 <amore012@ucr.edu>
2015-04-19 06:21:45 +0000

    Found Wifi, still struggling with the output. Don't know if I will be able to fix it,

--- week_3/ex4.py
+++ week_3/ex4.py
@@ -1,4 +1,4 @@
-print "Welcome to Program, comrad"
+print "Welcome to Program"
 
 #Import Libraries
 import matplotlib
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy.integrate import simps
 
-x = 0
+x = 0.
 def f(x):
     return (math.sin((100*x)**(.5)))**2
 
@@ -17,9 +17,21 @@ comparedTo = 0.4558325
 comparedTo = round(comparedTo, 6)
 print comparedTo
 result = 0.
-
-while round(result,6) != comparedTo:
-    i=0
-    for i in range(1)
-    
-    
+n = 0
+trapResult = 0.
+n = n + 1
+a = 0.
+b = 0.
+output = 0.
+while output != comparedTo:
+    for b in range(n):
+        a += 1/n
+        foxy = np.zeros(n)
+        foxy[b] = f(a) 
+    output = np.trapz(foxy, dx = 1/n)
+    output = round(output, 6)
+    error = abs(output-comparedTo/comparedTo)*100
+    if output != comparedTo:
+        print "Got %s, expected %s" %(output, comparedTo)
+        print "Error is %s" %(error)
+        n = n + 1
